d674f6f12879af2f359915c504a901fa684a24b5
Juan Ramirez <juanramirez@Juans-MacBook-Pro.local>
2022-11-21 22:42:29 +0100

    Eco.mio frontend challenge by Juan Ramirez

--- public/contentscript.js
+++ public/contentscript.js
@@ -1,11 +1,63 @@
-// console.log('Contentscript injected');
+let element = document.createElement('section');
+console.log(element);
 
-// let element = document.createElement('div');
-// const injectedText = document.createTextNode(`Extension-like element`);
+const imagePath = './eco.mio_icon_white.png';
 
-// element.appendChild(injectedText);
+const textArray = document.getElementsByClassName('makeStyles-question-24');
+const priceArray = document.getElementsByClassName('makeStyles-btb-27');
 
-// const parentElement = document.querySelector(
-//   '#root > div > div > div.makeStyles-mainPanel-3 > div.makeStyles-scrollbars-5 > div:nth-child(1) > div > section > div:nth-child(2) > h2',
-// );
-// parentElement.appendChild(element);
+const text = textArray[1].textContent;
+const price = priceArray[0].textContent;
+const injectedText = `${text}: ${price}`;
+
+element.innerHTML = `
+  <div id="box">
+    <img id="logo" src="${imagePath}" />
+    <p>${injectedText}</p>
+  </div>
+  <div id="detailedInformation">
+    <p>Greenhouse gas emissions can be reduced by making power on-site with renewables and other climate-friendly energy resources.</p>
+  </div>
+  `;
+
+const parentElement = document.querySelector(
+  '#root > div > div > div.makeStyles-mainPanel-3 > div.makeStyles-scrollbars-5 > div:nth-child(1) > div > section > div:nth-child(2) > h2',
+);
+
+parentElement.appendChild(element);
+
+//Start styling
+const section = element.style;
+const logo = document.getElementById('logo').style;
+const box = document.getElementById('box').style;
+const detailedInformation = document.getElementById('detailedInformation').style;
+
+section.backgroundColor = '#010682';
+section.color = '#ffffff';
+section.padding = '0px 10px';
+section.maxWidth = '100%';
+section.position = 'absolute';
+section.left = '55%';
+section.top = '7%';
+section.borderRadius = '10px';
+section.margin = '30px';
+
+logo.width = '20px';
+logo.height = '20px';
+logo.marginRight = '5px';
+
+box.display = 'flex';
+box.flexDirection = 'row';
+box.alignItems = 'center';
+
+detailedInformation.display = 'none';
+detailedInformation.fontWeight = '400';
+
+element.addEventListener('mouseover', () => {
+  detailedInformation.display = 'block';
+});
+
+element.addEventListener('mouseout', () => {
+  detailedInformation.display = 'none';
+});
+//End styling
--- src/tests/e2e/loginFlow.test.tsx
+++ src/tests/e2e/loginFlow.test.tsx
@@ -3,14 +3,47 @@
  */
 import 'expect-puppeteer';
 
-beforeEach(async () => {
-  await page.goto('http://localhost:3000/');
-});
+import puppeteer from 'puppeteer';
 
 describe('super awesome end to end tests', () => {
   jest.setTimeout(20000);
+  let browser: any;
+  let page: any;
 
-  test('if it does something', async () => {
-    // ...
+  beforeAll(async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000/extension');
   });
+
+  test('if header aka h1 is not null', async () => {
+    const header = await page.$eval(
+      '#root > div > div > div.makeStyles-mainPanel-3 > div.makeStyles-scrollbars-5 > div:nth-child(1) > div > section > h1',
+      (el: { textContent: string }) => el.textContent,
+    );
+    expect(header).not.toBeNull;
+  });
+
+  test('if header aka h1 contains DOM Manipulation', async () => {
+    const header = await page.$eval(
+      '#root > div > div > div.makeStyles-mainPanel-3 > div.makeStyles-scrollbars-5 > div:nth-child(1) > div > section > h1',
+      (el: { textContent: string }) => el.textContent,
+    );
+    expect(header).toContain('DOM Manipulation');
+  });
+
+  test('if box contains EUR symbol(€)', async () => {
+    const box = await page.$eval('#box > p', (el: { textContent: string }) => el.textContent);
+    expect(box).toContain('€');
+  });
+
+  test('if more detailed dummy information on climate change is not null', async () => {
+    const detailedDummyInfo = await page.$eval(
+      '#detailedInformation > p',
+      (el: { textContent: string }) => el.textContent,
+    );
+    expect(detailedDummyInfo).not.toBeNull;
+  });
+
+  afterAll(() => browser.close());
 });
